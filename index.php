fe57f6fd66faf530637b5e1039c8c5032e01aa3a
Palvi <palviatkle@gmail.com>
2025-06-11 17:48:02 -0700

    new project

---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="css/style.css" rel="stylesheet" type="text/css">
+  <title>Document</title>
+</head>
+<body>
+  <div id="container">
+    <div id="left-section">
+      <div id="heading">
+        <img src="images/facebook.png"/>
+
+      </div>
+      <div id="sub-heading">
+      Facebook helps you connect and share <br> with the people in your life.
+     
+      </div>
+    </div>
+    <div id="right-section">
+      <div id="form">
+        <div id="input-email">
+        <input class="input-button" type="text" placeholder="Email address or Phone number">
+        </div>
+        <div id="input-password">
+          <input class="input-button" type="text" placeholder="Password">
+          
+        </div>
+        <div id="login-button">
+          <button id="button-login">
+              <div id="login">
+            Log in
+              </div> 
+          </button>
+        </div>
+        <div id="forget-password">
+          <a class="forget" href="#">Forgotten password?</a>
+         </div>
+        <div id="separation-line">
+          <hr>
+        </div>
+        <div id="new-account">
+          <button id="button-new-account">
+              <div id="text-new-account">
+                Create new account
+              </div>
+          </button>
+      
+          
+        </div>
+      </div>
+      <div id="new-page">
+      <a class="newpage" href="#"><b>Create a Page</b> </a>for a celebrity, brand or business.
+      </div>
+    </div>
+    <div id="footer">
+      <div id="upper-content">
+        <a class="languages" href="#">
+          English (UK)
+        </a>
+        <a class="languages" href="#">
+        &nbsp; ਪੰਜਾਬੀ
+        </a>
+        <a class="languages" href="#">
+        &nbsp; हिन्दी
+        </a>
+        <a class="languages" href="#">
+        &nbsp; اردو
+        </a>
+        <a class="languages" href="#">
+        &nbsp;  मराठी
+        </a>
+        <a class="languages" href="#">
+        &nbsp; ગુજરાતી
+        </a>
+        <a class="languages" href="#">
+        &nbsp; বাংলা
+        </a>
+        <a class="languages" href="#">
+        &nbsp; தமிழ்
+        </a>
+        <a class="languages" href="#">
+        &nbsp; తెలుగు
+        </a>
+        <a class="languages" href="#">
+        &nbsp; ಕನ್ನಡ
+        </a>
+        <a class="languages" href="#">
+        &nbsp; മലയാളം
+        </a>
+      </div>
+        <div id="images">
+        <a href="#">
+        <img src="images/plus.png" height="25px" width="35px" />
+        </a>
+        </div>
+        <div id="footer-separation">
+        <hr id="line">
+        </div>
+        <div id="bottom-area">
+        <a class="languages" href="#">
+        Sign Up
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp; Log in
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp; Messenger
+        </a>
+
+        <a class="languages" href="#">
+        &nbsp; &nbsp; &nbsp;Facebook Lite
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp;  &nbsp; &nbsp;Video
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp; Places
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp; &nbsp; &nbsp;Games
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp; Marketplace
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp; Meta Pay
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp; &nbsp; Meta Store
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp; Meta Quest
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp; &nbsp; Meta AI
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp; Instagram
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp; &nbsp;  Threads
+        </a>
+        <br>
+        <a class="languages" href="#">
+        Fundraisers
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp;   Services
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp;   Voting  Information Centre
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp; &nbsp;  Privacy Policy
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp; &nbsp; &nbsp; Privacy Centre
+        </a>
+
+        <a class="languages" href="#">
+        &nbsp; &nbsp;   Groups
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp;  About
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp; &nbsp;  Create ad
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp;   Create Page
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp;   Developers
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp; &nbsp;  Careers
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp;   Cookies
+        </a>
+        <br>
+        <a class="languages" href="#">
+         AdChoices
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp; &nbsp;&nbsp;  Terms
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp;  &nbsp; Help
+        </a>
+        <a class="languages" href="#">
+        &nbsp; &nbsp;   Contact uploading and non-usersSettings
+        </a>
+        <br>
+        <br>
+        <a class="languages" href="#">
+        Meta © 2024
+        </a>
+
+        
+
+
+
+
+
+
+        </div>
+    </div>
+
+</div>
+</body>
+</html>
